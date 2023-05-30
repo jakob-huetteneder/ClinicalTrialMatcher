7c6eb4fd94cd9adee1177f23d6d76d52300ed559
Liliana Palankovska <e12122399@student.tuwien.ac.at>
2023-05-30 20:26:31 +0200

    [4.1] Partially implemented image view in Patient View

--- frontend/src/app/components/patient-detail/patient-detail.component.ts
+++ frontend/src/app/components/patient-detail/patient-detail.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Patient} from '../../dtos/patient';
 import {ActivatedRoute, Router} from '@angular/router';
 import {PatientService} from '../../services/patient.service';
+import {SafeUrl} from '@angular/platform-browser';
+import {FilesService} from '../../services/files.service';
 
 @Component({
   selector: 'app-patient-detail',
@@ -19,15 +21,21 @@ export class PatientDetailComponent implements OnInit{
     gender: undefined,
     birthdate: undefined
   };
-
+  examinationsImageVisible: boolean[] = [];
   id = -1;
   edit = false;
   loading = true;
 
+  image: SafeUrl = '';
+  imageFocus = false;
+  imageOriginal: SafeUrl = '';
+  imageName = '';
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private service: PatientService
+    private service: PatientService,
+    private fileService: FilesService
   ) {
   }
 
@@ -39,6 +47,8 @@ export class PatientDetailComponent implements OnInit{
         next: data => {
           this.patient = data;
           this.loading = false;
+          this.examinationsImageVisible = new Array(this.patient.examinations.length);
+          this.examinationsImageVisible.fill(false);
         },
         error: error => {
           console.error('Error, patient does not exist', error);
@@ -46,6 +56,23 @@ export class PatientDetailComponent implements OnInit{
         }
       });
     });
+  }
+
+  getIndexById(id: number): number {
+    return this.patient.examinations.findIndex(exam => exam.id === id);
+  }
+
+  zoomImage(event) {
+    this.imageFocus = !this.imageFocus;
+    if (this.imageFocus) {
+      const image = event.target;
+      image.classList.toggle('top-0');
+      image.classList.toggle('left-0');
+      image.classList.toggle('right-0');
+      image.classList.toggle('bottom-0');
+      image.classList.toggle('transition-all');
+      image.classList.toggle('duration-1100');
+    }
   }
 
   submit() {
@@ -59,6 +86,24 @@ export class PatientDetailComponent implements OnInit{
         //return this.router.navigate(['']);
       }
     });
+  }
+
+  public load(id: number, domelem: any): string {
+    let ret = '';
+    this.fileService.getById(id).subscribe({
+      next: data => {
+        const TYPED_ARRAY = new Uint8Array(data);
+        console.log(TYPED_ARRAY);
+        const STRING_CHAR = String.fromCharCode.apply(null, TYPED_ARRAY);
+        const base64String = btoa(STRING_CHAR);
+        ret = 'data:image/png;base64,' + base64String;
+        domelem.src = ret;
+      },
+      error: error => {
+        console.error('Error loading medical image', error);
+      }
+    });
+    return ret;
   }
 
   pulse(): string {
